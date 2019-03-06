276a1e3df8769d9ca8559e89e91fca28da1b6bfa
YuehWu1994 <yuehw@uci.edu>
2019-03-06 09:21:22 -0800

    try param3

--- cnn_bilstm_training.py
+++ cnn_bilstm_training.py
@@ -307,7 +307,7 @@ class CharCNN:
             
         self.get_model().fit(
             x_train, y_train, validation_data=(x_test, y_test), 
-            batch_size=batch_size, epochs=epochs, shuffle=shuffle, class_weight=self.class_weights_dict )
+            batch_size=batch_size, epochs=epochs, shuffle=shuffle)
         
 #         return self.model['doc_encoder']
 
@@ -339,7 +339,7 @@ if __name__ == '__main__':
     """
     We have to transform raw input training data and testing to numpy format for keras input
     """
-    char_cnn.load_data(200000)
+    char_cnn.load_data(100000)
     X_train, X_test, y_train, y_test = train_test_split(char_cnn.docs, char_cnn.labels, test_size=0.1, random_state=42)
     # X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     char_cnn.preprocess()
