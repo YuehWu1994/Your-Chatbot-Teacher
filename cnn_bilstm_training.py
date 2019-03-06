73a7c8f4f5899241d79c4a2e0c70cf15bc1250c9
YuehWu1994 <yuehw@uci.edu>
2019-03-06 08:32:18 -0800

    try param3

--- cnn_bilstm_training.py
+++ cnn_bilstm_training.py
@@ -48,7 +48,7 @@ class CharCNN:
             corpus = pkl.load(f)
             for c in corpus:
                 self.docs.append(c[0])
-                self.labels.append(c[2])
+                self.labels.append(c[1])
             # self.labels = np.array(labels)
         del corpus
         self.labels = self.labels[:size_limit]
@@ -338,7 +338,7 @@ if __name__ == '__main__':
     """
     We have to transform raw input training data and testing to numpy format for keras input
     """
-    char_cnn.load_data(200000)
+    char_cnn.load_data(100000)
     X_train, X_test, y_train, y_test = train_test_split(char_cnn.docs, char_cnn.labels, test_size=0.1, random_state=42)
     # X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     char_cnn.preprocess()
