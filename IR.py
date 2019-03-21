4b0a9c19a7d9da101b2ce28f7a19164ec7157d7f
William Kuo <lock3262000@gmail.com>
2019-03-20 23:07:52 -0700

    add class prediction to IR method

--- IR.py
+++ IR.py
@@ -10,8 +10,7 @@ from lstmEnc_DNN import lstmEncoder
 from evaluate import countBLEU
 from tfidfSentence import tfidfSentence
 import csv
-
-
+from keras.models import load_model
 
 if __name__ == "__main__": 
     REPEAT_WORD = False    
@@ -27,14 +26,19 @@ if __name__ == "__main__":
     total, correct = 100, 0
         
     bleu = countBLEU(lstm)
-    
+    classifier = load_model('./classifier.h5')
+    loss, accuracy = classifier.evaluate(X_test, y_test)
+    print("loss %f " % (loss*100))
+    print('Accuracy: %f' % (accuracy*100))
+    pred = classifier.predict(X_test)
+  
     with open('IR.csv', mode='w') as f:
         ir_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         ir_writer.writerow(['#id', 'question', 'most similar comment from all the subreddit', 'most similar comment from 1 the subreddit'])
     
         for i in range(total):
             c, q, a = tfidf.findMostSimilarOnAll(X_test[i])
-            c1, q1, a1 = tfidf.findMostSimilarOnOneClass(X_test[i], np.argmax(y_test[i]))
+            c1, q1, a1 = tfidf.findMostSimilarOnOneClass(X_test[i], np.argmax(pred[i]))
             print('\n')
     
             ir_writer.writerow([str(i), q, a, a1])
